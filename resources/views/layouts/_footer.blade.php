d5341ed1814bf0156ef8859a29254fa923113469
meiyiming <meiyiminghuiji@gmail.com>
2018-03-28 11:12:23 +0000

    基础布局

---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,8 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由 NoNo1 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
+
+    </div>
+</footer>
