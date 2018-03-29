4da7c53817390ad5f819b8c5c2751292fde06249
meiyiming <meiyiminghuiji@gmail.com>
2018-03-29 06:42:38 +0000

    文章编辑

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -33,22 +33,22 @@
                                     <input class="form-control" type="text" name="title" value="{{ old('title', $topic->title ) }}" placeholder="请填写标题" required/>
                               </div>
 
-                              <div class="form-group">
-                                   <select class="form-control" name="category_id" required>
-                                        <option value="" hidden disabled selected>请选择分类</option>
-                                            @foreach ($categories as $value)
-                                                <option value="{{ $value->id }}">{{ $value->name }}</option>
-                                            @endforeach
-                                   </select>
-                              </div>
+                             <div class="form-group">
+                                 <select class="form-control" name="category_id" required>
+                                     <option value="" hidden disabled {{ $topic->id ? '' : 'selected' }}>请选择分类</option>
+                                     @foreach ($categories as $value)
+                                         <option value="{{ $value->id }}" {{ $topic->category_id == $value->id ? 'selected' : '' }}>{{ $value->name }}</option>
+                                     @endforeach
+                                 </select>
+                             </div>
 
-                                    <div class="form-group">
-                                        <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
-                                    </div>
+                             <div class="form-group">
+                                   <textarea name="body" class="form-control" id="editor" rows="3" placeholder="请填入至少三个字符的内容。" required>{{ old('body', $topic->body ) }}</textarea>
+                             </div>
 
-                                    <div class="well well-sm">
-                                        <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
-                                    </div>
+                             <div class="well well-sm">
+                                  <button type="submit" class="btn btn-primary"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span> 保存</button>
+                             </div>
                            </form>
                 </div>
             </div>
@@ -71,6 +71,14 @@
         $(document).ready(function(){
             var editor = new Simditor({
                 textarea: $('#editor'),
+                upload: {
+                    url: '{{ route('topics.upload_image') }}',
+                    params: { _token: '{{ csrf_token() }}' },
+                    fileKey: 'upload_file',
+                    connectionCount: 3,
+                    leaveConfirm: '文件上传中，关闭此页面将取消上传。'
+                },
+                pasteImage: true,
             });
         });
     </script>
