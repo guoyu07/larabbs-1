02c73e2d4efbd9e0634da6e8f85dca9be53dc985
meiyiming <meiyiminghuiji@gmail.com>
2018-03-30 07:57:46 +0000

    6.3 消息通知 新建路由

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,10 +4,23 @@ namespace App\Models;
 
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Auth;
 
 class User extends Authenticatable
 {
-    use Notifiable;
+    use Notifiable {
+        notify as protected laravelNotify;
+    }
+
+    public function notify($instance)
+    {
+        // 如果要通知的人是当前用户，就不必通知了
+        if($this->id == Auth::id()){
+            return ;
+        }
+        $this->increment('notification_count');
+        $this->laravelNotify($instance);
+    }
 
     /**
      * The attributes that are mass assignable.
@@ -26,6 +39,11 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 
     public function topics()
     {
