5a326fa09ba7edf21db8fa35c7d6c1f8e736b42b
meiyiming <meiyiminghuiji@gmail.com>
2018-03-31 14:24:48 +0000

    回复通知

--- app/Notifications/TopicReplied.php
+++ app/Notifications/TopicReplied.php
@@ -11,36 +11,27 @@ use App\Models\Reply;
 class TopicReplied extends Notification
 {
     use Queueable;
+
     public $reply;
 
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
     public function __construct(Reply $reply)
     {
         // 注入回复实体，方便 toDatabase 方法中的使用
         $this->reply = $reply;
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
+        // 开启通知的频道
         return ['database'];
     }
 
     public function toDatabase($notifiable)
     {
         $topic = $this->reply->topic;
-        $link = $topic->link(['#reply' . $this->reply->id]);
+        $link =  $topic->link(['#reply' . $this->reply->id]);
 
-        // 存入数据库中的数据
+        // 存入数据库里的数据
         return [
             'reply_id' => $this->reply->id,
             'reply_content' => $this->reply->content,
